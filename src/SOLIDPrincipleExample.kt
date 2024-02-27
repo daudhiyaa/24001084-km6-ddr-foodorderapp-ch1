d0f9936d8b5b40171f7c93c6e4187dc8fbd9a3b9
Daud Dhiya <daud.dhiya31@gmail.com>
2024-02-27 20:33:26 +0700

    feat: SOLID Principle Example

---
+++ src/SOLIDPrincipleExample.kt
@@ -0,0 +1,90 @@
+/* Open - Closed Principle */
+open class IronManSuit() {
+    // opened function
+    open fun blast() {
+        println("Blast using Iron Man Suit")
+    }
+
+    // closed function
+    fun activateJarvis() {
+        println("Jarvis is activated")
+        Thread.sleep(1000)
+    }
+}
+
+class WarMachineSuit() : IronManSuit () {
+    override fun blast() {
+        println("Blast using War Machine Suit")
+    }
+}
+
+/* Dependency Inversion */
+class FirebaseAnalytics() {
+    fun log(e: String){
+        println("Logging to Firebase : Event Name : $e")
+        Thread.sleep(2000)
+        println("Logging Success!")
+    }
+}
+
+class NewRelicAnalytics() {
+    fun log(e: String){
+        println("Logging to New Relic : Event Name : $e")
+        Thread.sleep(2000)
+        println("Logging Success!")
+    }
+}
+
+class MoEngageAnalytics() {
+    fun log(e: String){
+        println("Logging to Mo Engage : Event Name : $e")
+        Thread.sleep(2000)
+        println("Logging Success!")
+    }
+}
+
+interface Analytics {
+    fun logEvent(e: String)
+}
+
+// agnostic class
+class AppAnalytics : Analytics {
+    private var firebaseAnalytics = FirebaseAnalytics()
+    private var newRelicAnalytics = NewRelicAnalytics()
+    private var moEngageAnalytics = MoEngageAnalytics()
+
+    override fun logEvent(e: String) {
+        firebaseAnalytics.log(e)
+        newRelicAnalytics.log(e)
+        moEngageAnalytics.log(e)
+    }
+
+}
+
+class LoginPage(private val analytics: FirebaseAnalytics) {
+    fun openPage(pageName: String) {
+        println("Open $pageName Page...")
+        analytics.log(pageName)
+    }
+}
+
+class HomePage(private val analytics: FirebaseAnalytics) {
+    fun openPage(pageName: String) {
+        println("Open $pageName Page...")
+        analytics.log(pageName)
+    }
+}
+
+class App {
+    fun run () {
+        LoginPage(FirebaseAnalytics()).openPage("Login")
+        HomePage(FirebaseAnalytics()).openPage("Home")
+    }
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            App().run()
+        }
+    }
+}
